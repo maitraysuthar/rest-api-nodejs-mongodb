f59118ebf1e2f94349f2ee6fd5149726b7f50796
Maitray Suthar <maitray.suthar@cygnetinfotech.com>
2019-07-23 19:09:54 +0530

    Add helper and manage 404 page not found.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var apiResponse = require('./helpers/apiResponse');
+
 var app = express();
 
 app.use(logger('dev'));
@@ -16,5 +18,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// throw 404 if URL not found
+app.get("*", function(req, res) {
+    return apiResponse.notFoundResponse(res, 'Page not found');
+});
 
 module.exports = app;
---
+++ helpers/apiResponse.js
@@ -0,0 +1,32 @@
+exports.successResponse = function (res, msg) {
+    var data = {
+        status: 1,
+        message: msg
+    }
+    return res.status(200).json(data);
+};
+
+exports.successResponseWithData = function (res, msg, data) {
+    var data = {
+        status: 1,
+        message: msg,
+        data: data
+    }
+    return res.status(200).json(data);
+}
+
+exports.ErrorResponse = function (res, msg) {
+    var data = {
+        status: 0,
+        message: msg,
+    }
+    return res.status(500).json(data);
+}
+
+exports.notFoundResponse = function (res, msg) {
+    var data = {
+        status: 0,
+        message: msg,
+    }
+    return res.status(404).json(data);
+}
