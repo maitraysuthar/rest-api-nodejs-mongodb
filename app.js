d425dd778e674dc345394ff5a4f7722f7c4415c9
Maitray Suthar <maitray.suthar@cygnetinfotech.com>
2019-07-24 17:41:47 +0530

    Add database connection.

--- app.js
+++ app.js
@@ -8,6 +8,24 @@ var usersRouter = require('./routes/users');
 
 var apiResponse = require('./helpers/apiResponse');
 
+/* DB connection */
+var mongoose = require('mongoose');
+
+var mongoDB = 'mongodb://127.0.0.1/rest-api-nodejs-mongodb';
+mongoose.connect(mongoDB, { useNewUrlParser: true }).then(() => {
+    console.log("Connected to %s", mongoDB);
+  })
+  .catch(err => {
+    console.error("App starting error:", err.message);
+    process.exit(1);
+  });
+var db = mongoose.connection;
+// db.on('error', (error)=>{
+//     //console.error('MongoDB connection error:'error);
+//     console.error.bind(console, 'MongoDB connection error:')
+//     process.exit(0);
+// });
+
 var app = express();
 
 app.use(logger('dev'));
@@ -20,7 +38,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // throw 404 if URL not found
-app.get("*", function(req, res) {
+app.all("*", function(req, res) {
     return apiResponse.notFoundResponse(res, 'Page not found');
 });
 
