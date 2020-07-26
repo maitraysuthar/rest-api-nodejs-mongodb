14bdf5b5dbc1c64d418a9a28c383c1caf537fd2e
aliartiza75 <aliartiza75@yahoo.com>
2020-07-26 13:49:36 +0500

    [add-docker-support] fix issues

--- app.js
+++ app.js
@@ -11,23 +11,26 @@ var cors = require("cors");
 // DB connection
 var MONGODB_URL = process.env.MONGODB_URL;
 var mongoose = require("mongoose");
-
-mongoose.connect(MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true }).then(() =>{
-	console.log("Connection to mongodb has been successful.");
-}).catch((err) => {
-	console.log("Unable to connect with mongodb");
-	console.error(err);
-	process.exit(1);
-});
-
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+	//don't show the log when it is test
+	if(process.env.NODE_ENV !== "test") {
+		console.log("Connected to %s", MONGODB_URL);
+		console.log("App is running ... \n");
+		console.log("Press CTRL + C to stop the process. \n");
+	}
+})
+	.catch(err => {
+		console.error("App starting error:", err.message);
+		process.exit(1);
+	});
+var db = mongoose.connection;
 
 var app = express();
+
 //don't show the log when it is test
 if(process.env.NODE_ENV !== "test") {
 	app.use(logger("dev"));
 }
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
