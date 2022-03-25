a09c60ffbf8f125b312c650c181e921094717359
ThienCao <caothien029@gmail.com>
2022-03-25 10:06:27 +0700

    add middlewares check user had blocked

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -1,6 +1,12 @@
 const jwt = require("jsonwebtoken");
 const RefreshToken = require("../models/RefreshTokenModel");
-
+const User = require("../models/UserModel");
+/**
+ * Verify session is logout
+ * @param {*} req 
+ * @param {*} payload 
+ * @param {*} done 
+ */
 function isRevoked(req, payload, done) {
 	RefreshToken.findOne({ user: payload._id }).then((foundRefreshToken) => {
 		if (!foundRefreshToken) {
@@ -11,6 +17,23 @@ function isRevoked(req, payload, done) {
 		return done(err);
 	}));
 }
+/**
+ * Verify session is blocked because super admin block user
+ * @param {*} req 
+ * @param {*} payload 
+ * @param {*} done 
+ */
+function isBlocked(req, payload, done) {
+	User.findById(payload._id).then(user => {
+		if (!user || !user.status) {
+			return done(true);
+		}
+		return done(false);
+	}, (err) => {
+		return done(err);
+	});
+}
+
 function authenticateToken(req, res, next) {
 	const authHeader = req.headers["authorization"];
 	const token = authHeader && authHeader.split(" ")[1];
@@ -25,7 +48,10 @@ function authenticateToken(req, res, next) {
 
 		isRevoked(req, user, (error) => {
 			if (error) return res.sendStatus(403);
-			next();
+			isBlocked(req, user, (error) => {
+				if (error) return res.sendStatus(403);
+				next();
+			});
 		});
 	});
 }
