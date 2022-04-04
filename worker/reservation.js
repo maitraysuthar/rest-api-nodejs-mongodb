99feacf92948d909021fc8947d5026f5cdd24beb
ThienCao <caothien029@gmail.com>
2022-04-04 15:02:49 +0700

    add job auto checkout

--- worker/reservation.js
+++ worker/reservation.js
@@ -3,7 +3,9 @@ const moment = require("moment");
 
 const Reservation = require("../models/ReservationModel");
 const { RESERVATION_STATUS } = require("../constants/index");
-
+/**
+ * Job clean pendding payment
+ */
 cron.schedule(
     "0 * * * *",
     () => {
@@ -16,7 +18,34 @@ cron.schedule(
         }, {
             status: RESERVATION_STATUS.REJECTED
         }).then(() => {
-            console.log("Reject reservation expired.", new Date());
+            console.log("Reject reservation expired.", moment(moment.now()).toDate());
+        }, (error) => {
+            console.error(error);
+        });
+    },
+    {
+        scheduled: true,
+        timezone: "Asia/Ho_Chi_Minh"
+    }
+);
+
+
+/**
+ * Job checkout
+ */
+cron.schedule(
+    "0 0 16 * * *",
+    () => {
+        const now = moment().toDate();
+        Reservation.updateMany({
+            checkOut: {
+                $lte: now
+            },
+            status: RESERVATION_STATUS.PENDING_COMPLETED
+        }, {
+            status: RESERVATION_STATUS.COMPLETED
+        }).then(() => {
+            console.log("AUTO checkout reservation.", moment(moment.now()).toDate());
         }, (error) => {
             console.error(error);
         });
