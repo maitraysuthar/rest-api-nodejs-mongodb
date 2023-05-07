8ab1f2110573c430d1d2bfff429a3cb29540afdc
Md. Muhtasim Fuad Fahim <69357704+mdmuhtasimfuadfahim@users.noreply.github.com>
2023-05-07 22:28:45 +0600

    Issue #57 errors solved

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -2,7 +2,9 @@ const jwt = require("express-jwt");
 const secret = process.env.JWT_SECRET;
 
 const authenticate = jwt({
-	secret: secret
+	secret: secret,
+	credentialsRequired: false,
+	algorithms: ['HS256']
 });
 
 module.exports = authenticate;
